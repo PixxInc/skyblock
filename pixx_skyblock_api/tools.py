fd65f66c411cfcd3b752e86ceba19c32ca1bb857
PixxInc <87732373+PixxInc@users.noreply.github.com>
2023-10-12 18:39:57 +0200

    Update tools.py

--- pixx_skyblock_api/tools.py
+++ pixx_skyblock_api/tools.py
@@ -5,6 +5,33 @@ import requests
 from io import BytesIO
 import random
 
+emotes={
+    # Mayors
+    "Aatrox": "<:Aatrox_Sprite:1161710871555346452>",
+    "Cole": "<:Cole_Sprite:1161710879327408169>",
+    "Derpy": "<:Derpy_Sprite:1161710857957421147>",
+    "Diana": "<:Diana_Sprite:1161710876605284524>",
+    "Diaz": "<:Diaz_Sprite:1161710874650751150>",
+    "Finnegan": "<:Finnegan_Sprite:1161710841347985418>",
+    "Foxy": "<:Foxy_Sprite:1161710865125474384>",
+    "Marina": "<:Marina_Sprite:1161710862403371088>",
+    "Paul": "<:Paul_Sprite:1161710860679512215>",
+    "Scorpius": "<:Scorpius_Sprite:1161710843919081542>",
+    "Villager": "<:Villager_Sprite:1161710853301751939>",
+    # Other
+    "Fairy soul": "<:fairy_soul:1161953288686678108>",
+    "Coins": "<:Coins:1161952966442496080>",
+    "XP": "<:experience_bottle:1161952849601773588>",
+    # Rarity Emojis
+    "Common": "<:common:1161956252973289542>",
+    "Uncommon": "<:uncommon:1161956283356807229>",
+    "Rare": "<:rare:1161956310238122034>",
+    "Epic": "<:epic:1161956344652365874>",
+    "Legendary": "<:legendary:1161956395336335460>",
+    "Mythic": "<:mythic:1161956437623316492>",
+    "Divine": "<:divine:1161956434838290473>",
+    "Special": "<:special:1161956433332551700>",
+}
 
 async def convert_usename_to_uuid(username):
     url = f"https://playerdb.co/api/player/minecraft/{username}"
