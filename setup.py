b6ac8592123b95f8b68be29cfd628ec750ace9b3
PixxInc <87732373+PixxInc@users.noreply.github.com>
2023-10-12 18:03:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='pixx_skyblock_api',
-    version='0.1',
+    name="pixx_skyblock_api",
+    version="0.0.2",
+    author="Pixx",
+    author_email="admin@pixx.pics",
+    description="Shitty api wrapper for the official hypixel skyblock api",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PixxInc/skyblock"
     packages=find_packages(),
-    install_requires=[
-        'discord', 'aiohttp', 'PIL', 'requests'
-    ],
-    entry_points={
-        'console_scripts': [
-            'my_package = my_package.main:main_func'
-        ]
-    }
+    install_requires=requirements,
+    python_requires='>=3.7',
 )
