529517f25eef27e3032a2d9059ba8fed5a3500cd
PixxInc <87732373+PixxInc@users.noreply.github.com>
2023-10-13 20:38:15 +0200

    Create res.py

---
+++ pixx_skyblock_api/res.py
@@ -0,0 +1,29 @@
+
+emotes={
+    # Mayors
+    "Aatrox": "<:Aatrox_Sprite:1161710871555346452>",
+    "Cole": "<:Cole_Sprite:1161710879327408169>",
+    "Derpy": "<:Derpy_Sprite:1161710857957421147>",
+    "Diana": "<:Diana_Sprite:1161710876605284524>",
+    "Diaz": "<:Diaz_Sprite:1161710874650751150>",
+    "Finnegan": "<:Finnegan_Sprite:1161710841347985418>",
+    "Foxy": "<:Foxy_Sprite:1161710865125474384>",
+    "Marina": "<:Marina_Sprite:1161710862403371088>",
+    "Paul": "<:Paul_Sprite:1161710860679512215>",
+    "Scorpius": "<:Scorpius_Sprite:1161710843919081542>",
+    "Villager": "<:Villager_Sprite:1161710853301751939>",
+    # Other
+    "Fairy": "<:fairy_soul:1161953288686678108>",
+    "Coins": "<:Coins:1161952966442496080>",
+    "XP": "<:experience_bottle:1161952849601773588>",
+    # Rarity Emojis
+    "Common": "<:common:1161956252973289542>",
+    "Uncommon": "<:uncommon:1161956283356807229>",
+    "Rare": "<:rare:1161956310238122034>",
+    "Epic": "<:epic:1161956344652365874>",
+    "Legendary": "<:legendary:1161956395336335460>",
+    "Mythic": "<:mythic:1161956437623316492>",
+    "Divine": "<:divine:1161956434838290473>",
+    "Special": "<:special:1161956433332551700>",
+}
+
